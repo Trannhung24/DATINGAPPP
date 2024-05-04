89af83c303aca506cab337c0b0c6d3bcc5b8cc7f
Trannhung24 <137737181+Trannhung24@users.noreply.github.com>
2024-05-04 21:59:38 +0700

    End of section 8

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,7 +5,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Controllers;
 using API.Data;
+using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -16,27 +19,31 @@ namespace API.Controller
     [Route("api/[Controller]")]
     public class UsersController : BaseApiController
     {
-        private readonly DataContext _context;
-        public UsersController(DataContext context)
+        private readonly IMapper _mapper;
+
+        private readonly IUserRepository _userRepository;
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
-            _context = context;
+            _mapper = mapper;
+            _userRepository = userRepository;
+
         }
 
         [HttpGet]
-        [AllowAnonymous]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-            return await _context.Users.ToListAsync();
-
+            var users = await _userRepository.GetMembersAsync();
+            return Ok(users);
         }
-        [Authorize]
 
-        [HttpGet("{Id}")]
-        public async Task<ActionResult<AppUser>> GetUsers(int Id)
+        [HttpGet("{username}")]
+        public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
-            return await _context.Users.FindAsync(Id);
-
+            return await _userRepository.GetMemberAsync(username);;
+         
         }
+
+
     }
 
 
--- API/Program.cs
+++ API/Program.cs
@@ -1,41 +1,50 @@
-using System.Runtime.CompilerServices;
-using System.Text;
 using API.Data;
 using API.Extensions;
-using API.Interfaces;
 using API.Middleware;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-IConfiguration configuration = builder.Configuration;
-builder.Services.AddApplicationServices(configuration);
+IConfiguration config = builder.Configuration;
+
+builder.Services.AddApplicationServices(config);
+
 builder.Services.AddControllers();
 builder.Services.AddCors();
-builder.Services.AddIdentityServices(configuration);
+builder.Services.AddIdentityServices(config);
 var app = builder.Build();
 
-app.UseMiddleware<ExceptionMiddleware>();
-
 app.UseHttpsRedirection();
+
 app.UseRouting();
+
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
-
-app.UseCors();
-
-app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseMiddleware<ExceptionMiddleware>();
 
-app.Run();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch(Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
+
+await app.RunAsync();
