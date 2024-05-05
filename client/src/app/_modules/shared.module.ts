ecb5793f5d164f01b48a74d9cf8b909a8bf8513d
Trannhung24 <137737181+Trannhung24@users.noreply.github.com>
2024-05-05 22:49:11 +0700

    end of section 9

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserModule } from '@angular/platform-browser';
+import{TabsModule} from 'ngx-bootstrap/tabs'
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 
 @NgModule({
   declarations: [],
@@ -12,11 +14,14 @@ import { BrowserModule } from '@angular/platform-browser';
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right'
     }),
-    
+    TabsModule.forRoot(),
+    NgxGalleryModule
   ],
   exports: [
     BsDropdownModule,
     ToastrModule,
+    TabsModule,
+    NgxGalleryModule
   ]
 })
 export class SharedModule { }
